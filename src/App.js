339a6f1da896e62638696f2f6e5a6cce1e72812b
karlaevelize <k.evelize@gmail.com>
2021-10-20 13:56:58 +0200

    New title componnent

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Title from './components/Title';
 
 function App() {
   return (
     <div className="App">
       <h1>Collaborating in GitHub</h1>
+      <Title/>
     </div>
   );
 }
